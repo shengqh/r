dcf3553eb7bb872b8c8ba9470daa888690043fd1
shengqh <shengqh@gmail.com>
2014-02-19 11:29:36 -0800

    metavalid

---
+++ papers/2014_metavalid/buildpackage.R
@@ -0,0 +1,13 @@
+# TODO: Add comment
+# 
+# Author: shengq1
+###############################################################################
+
+
+setwd("E:\\sqh\\programs")
+library(roxygen2)
+roxygenize('metavalid')
+#roxygenize('KEGGprofile')
+#roxygenize('MultiRankSeq')
+#roxygenize('RnaSeqSampleSize')
+
