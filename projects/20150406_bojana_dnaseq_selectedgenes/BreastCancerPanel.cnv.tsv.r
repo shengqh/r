74b1517c604c006fcf2f904e23323313b8ae25d5
shengqh <shengqh@gmail.com>
2015-07-16 11:15:43 -0500

    clinical project

--- projects/20150406_bojana_dnaseq_selectedgenes/BreastCancerPanel.cnv.tsv.r
+++ projects/20150406_bojana_dnaseq_selectedgenes/BreastCancerPanel.cnv.tsv.r
@@ -5,10 +5,20 @@ title<-"Number of Copy Number Variation detected in capture range"
 
 library(reshape)
 
-md<-melt(data)
+md<-melt(data, id="Gene")
 md[is.na(md$value),"value"]<-0
-#md<-md[!is.na(md$value),]
-colnames(md)<-c("Gene", "Sample", "CNVCount")
+md$Type<-unlist(lapply(md$value, function(x){
+  if(x > 0){
+    "DUPLICATION"
+  }else if(x < 0){
+    "DELETION"
+  }else{
+    ""
+  }
+}))
+
+md$Type<-factor(md$Type, levels=c("DUPLICATION", "DELETION", ""))
+colnames(md)<-c("Gene", "Sample", "Coverage","Type")
 
 response<-read.table("response.tsv", sep="\t", header=T, stringsAsFactor=F)
 md$Sample <- factor(md$Sample, levels = response$SampleID)
@@ -18,21 +28,22 @@ names(colors)<-c("PCR", "NO-PCR", "NEAR-PCR")
 
 library(ggplot2)
 
-png(file=paste0(inputfile, ".png"), width=8000, height=16000, res=300)
-ggplot(data=md, aes(x=Sample, y=Gene)) +       
-  geom_point(aes(size=CNVCount), colour="darkblue") +
-  ggtitle(title) +
-  theme(axis.text.x = element_text(angle=90, vjust=1, size=11, hjust=1, face="bold", color=colors[response$Response]),
-        axis.text.y = element_text(size=11, face="bold"))
+png(file=paste0(inputfile, ".png"), width=6000, height=12000, res=300)
+ggplot(md, aes(Sample, Gene))+  
+  geom_tile(data=md, aes(fill=Type, alpha=Coverage), color="white") +
+  scale_fill_manual(values=c("blue", "red", "white")) +
+  theme(axis.text.x = element_text(angle=90, vjust=0.5, size=11, hjust=1, face="bold", color=colors[response$Response]),
+        axis.text.y = element_text(size=11, face="bold")) +
+  coord_equal()  
 dev.off()
 
 mygenes<-read.table("mygenes.txt")
 mydata<-md[md$Gene %in% mygenes$V1,]
 png(file=paste0(inputfile, ".mygene.png"), width=8000, height=2000, res=300)
-ggplot(data=mydata, aes(x=Sample, y=Gene)) +       
-  geom_point(aes(size=CNVCount), colour="darkblue") +
-  ggtitle(title) +
-  theme(axis.text.x = element_text(angle=90, vjust=1, size=11, hjust=1, face="bold", color=colors[response$Response]),
-        axis.text.y = element_text(size=11, face="bold"))
+ggplot(mydata, aes(Sample, Gene))+  
+  geom_tile(data=mydata, aes(fill=Type, alpha=Coverage), color="white") +
+  scale_fill_manual(values=c("blue", "white")) +
+  theme(axis.text.x = element_text(angle=90, vjust=0.5, size=11, hjust=1, face="bold", color=colors[response$Response]),
+        axis.text.y = element_text(size=11, face="bold")) +
+  coord_equal()  
 dev.off()
-
